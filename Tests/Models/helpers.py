972d446d1ec0a882561aa127b6ff0eed4b807729
Jack Dane <jackd98.jd@gmail.com>
2022-01-16 22:31:05 +0000

    UnitTests
    UnitTests for iterate function decorator

---
+++ Tests/Models/helpers.py
@@ -0,0 +1,41 @@
+
+from unittest import TestCase
+from unittest.mock import MagicMock, Mock
+
+from Models.helpers import iterateGrid
+
+
+class FunctionMockWrap(Mock):
+
+    def __init__(self):
+        super(FunctionMockWrap, self).__init__()
+        self.rows = 10
+        self.columns = 10
+        self.functionCallCount = 0
+        self.notifiedObservers = False
+
+    @iterateGrid
+    def function(self, x, y):
+        self.functionCallCount += 1
+
+    def notifyObservers(self):
+        self.notifiedObservers = True
+
+
+class Test_iterateGrid(TestCase):
+
+    def test(self):
+        objectMock = FunctionMockWrap()
+
+        objectMock.function()
+
+        self.assertEqual(objectMock.functionCallCount, 100)
+        self.assertFalse(objectMock.notifiedObservers)
+
+    def test_update(self):
+        objectMock = FunctionMockWrap()
+
+        objectMock.function(update=True)
+
+        self.assertEqual(objectMock.functionCallCount, 100)
+        self.assertTrue(objectMock.notifiedObservers)
